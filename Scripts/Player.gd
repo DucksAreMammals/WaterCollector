b4a8f9f8cc17691c57e4c884dbfe7d9b6eab714d
DucksAreMammals <34693858+DucksAreMammals@users.noreply.github.com>
2021-11-04 00:52:25 -0700

    Mostly made movement. It can be improved

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -1,38 +1,51 @@
 extends KinematicBody2D
 
-export var move_speed := 200
+export var move_speed := 400
+export var air_speed := 25
+export var jump_speed := 1000
 export var gravity := 2000
-export var jump_speed := 750
+export var air_resistance = 0.94
 
 var velocity := Vector2.ZERO
+var snapping := Vector2.DOWN * 15
 
 func _process(delta: float) -> void:
 	change_animation()
 
 func _physics_process(delta: float) -> void:
-	velocity.x = 0
-
-	if Input.is_action_pressed("right"):
-		velocity.x += move_speed
-	if Input.is_action_pressed("left"):
-		velocity.x -= move_speed
+	if is_on_floor():
+		velocity.x = 0
+		
+		if Input.is_action_pressed("right"):
+			velocity.x = move_speed
+		if Input.is_action_pressed("left"):
+			velocity.x = move_speed
+	else:
+		velocity.x *= air_resistance
+		
+		if Input.is_action_pressed("right"):
+			velocity.x += air_speed
+		if Input.is_action_pressed("left"):
+			velocity.x -= air_speed
 
 	velocity.y += gravity * delta
 
-	velocity = move_and_slide_with_snap(velocity, Vector2.DOWN * 5, Vector2.UP, true)
+	velocity = move_and_slide_with_snap(velocity, snapping, Vector2.UP, true)
 	
-	if Input.is_action_just_pressed("jump"):
-		if is_on_floor():
-			velocity.y = -jump_speed # negative Y is up in Godot
+	if is_on_floor():
+		if Input.is_action_just_pressed("jump"):
+			velocity.y = -jump_speed
+			snapping = Vector2.ZERO
+		else:
+			snapping = Vector2.DOWN * 15
 
 func change_animation():
-	# face left or right
 	if velocity.x > 0:
 		$AnimatedSprite.flip_h = false
 	elif velocity.x < 0:
 		$AnimatedSprite.flip_h = true
 	
-	if abs(velocity.y) > 0.5:
+	if !is_on_floor():
 		$AnimatedSprite.play("jump")
 	elif abs(velocity.x) > 0.2:
 		$AnimatedSprite.play("run")
