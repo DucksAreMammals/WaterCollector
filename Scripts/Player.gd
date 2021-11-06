03f15e537bb72a6b3ddd112d5272983e98b25be2
DucksAreMammals <34693858+DucksAreMammals@users.noreply.github.com>
2021-11-06 14:04:22 -0700

    Screen shake is now handled by the camera and works properly

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -9,17 +9,14 @@ export var jump_speed := 1000.0
 export var air_resistance := 0.8
 export var gravity := 1700.0
 
-export var x_bounce_speed := 750.0
-export var y_bounce_speed := 500.0
+export var invincibility_duration := 0.5
 
-export var shake_amount := 5.0
-export var shake_decay := 10.0
-export var shake_speed := 0.00008
+export var x_hurt_bounce_speed := 750.0
+export var y_hurt_bounce_speed := 500.0
 
-var camera_shake_amount := 0.0
+export var x_attack_bounce_speed := 0.0
+export var y_attack_bounce_speed := 500.0
 
-export var max_roll := 0.2
-export var max_offset := 20
 
 export var health := 3
 
@@ -28,27 +25,11 @@ var move_speed
 var velocity := Vector2.ZERO
 var snapping := Vector2.DOWN * 15
 var invincible_until := -1.0
-var invincibility_duration := 0.5
 
 var bounce := Vector2.ZERO
 
-onready var noise = OpenSimplexNoise.new()
-
-func _ready():
-	randomize()
-	noise.seed = randi()
-	noise.period = 4
-	noise.octaves = 2
-
-
 func _process(_delta: float) -> void:
 	change_animation()
-
-	if camera_shake_amount > 0:
-		$Camera2D.rotation = max_roll * camera_shake_amount * noise.get_noise_2d(noise.seed, OS.get_ticks_usec() * shake_speed)
-		$Camera2D.offset.x = max_offset * camera_shake_amount * noise.get_noise_2d(noise.seed*2, OS.get_ticks_usec() * shake_speed)
-		$Camera2D.offset.y = max_offset * camera_shake_amount * noise.get_noise_2d(noise.seed*3, OS.get_ticks_usec() * shake_speed)
-		camera_shake_amount -= _delta * shake_decay
 
 func _physics_process(delta: float) -> void:
 	_move(delta)
@@ -86,8 +67,6 @@ func _move(delta):
 		velocity.x *= air_resistance
 		velocity.x += (Input.get_action_strength("right") - Input.get_action_strength("left")) * air_speed * delta
 	
-	velocity.y += gravity * delta
-	
 	if Input.is_action_just_pressed("jump"):
 		if is_on_floor():
 			velocity.y = -jump_speed
@@ -99,7 +78,9 @@ func _move(delta):
 		velocity += bounce
 		bounce = Vector2.ZERO
 		snapping = Vector2.ZERO
-
+	
+	velocity.y += gravity * delta
+	
 	velocity = move_and_slide_with_snap(velocity, snapping, Vector2.UP, true)
 
 func _collide():
@@ -107,20 +88,22 @@ func _collide():
 		var collision = get_slide_collision(i)
 
 		if collision.collider.is_in_group("enemy"):
-			camera_shake_amount = shake_amount
-
 			if collision.normal.y < 0:
 				collision.collider.hit()
 
-				bounce.y = -y_bounce_speed
+				bounce.y = -y_hurt_bounce_speed
+				bounce.x = x_attack_bounce_speed
+				
+				$Camera.shake()
 			else:
 				_hit()
 				
-				bounce.y = -y_bounce_speed
-				bounce.x = collision.normal.x * x_bounce_speed
+				bounce.y = -y_hurt_bounce_speed
+				bounce.x = collision.normal.x * x_hurt_bounce_speed
+				
+				$Camera.shake()
 
 func _hit():
-	# TODO: Shake camera
 	if invincible_until < OS.get_ticks_usec():
 		invincible_until = OS.get_ticks_usec() + invincibility_duration * 1000000
 
