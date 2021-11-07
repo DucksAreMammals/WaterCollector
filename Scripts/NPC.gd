6d1c7fd73de055b4241f49cdbec249384e7889ad
DucksAreMammals <34693858+DucksAreMammals@users.noreply.github.com>
2021-11-07 10:44:18 -0600

    Added NPC text box animation

--- Scripts/NPC.gd
+++ Scripts/NPC.gd
@@ -3,11 +3,8 @@ extends Node2D
 
 func _on_Area2D_body_entered(body):
 	if (body.is_in_group("player")):
-		$TextBox.visible = true
-
-
-
+		$AnimationPlayer.play("out")
 
 func _on_Area2D_body_exited(body):
 	if (body.is_in_group("player")):
-		$TextBox.visible = false
+		$AnimationPlayer.play("in")
