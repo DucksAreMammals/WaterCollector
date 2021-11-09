3615a1996441d3e45805f67e3e6d413e49cd8717
DucksAreMammals <34693858+DucksAreMammals@users.noreply.github.com>
2021-11-09 00:50:46 -0600

    Added level functionality

--- Scripts/GameLogic.gd
+++ Scripts/GameLogic.gd
@@ -1,9 +1,13 @@
 extends Node2D
 
+export (String, FILE) var next_level
+
 func _on_WaterFaucet_finish():
 # warning-ignore:return_value_discarded
-	 get_tree().reload_current_scene()
-
+	if next_level:
+		get_tree().change_scene(next_level)
+	else:
+		get_tree().reload_current_scene()
 
 func _on_Player_die():
 # warning-ignore:return_value_discarded
