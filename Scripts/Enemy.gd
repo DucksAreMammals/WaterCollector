7f4d72ecd359f28d9229e4220b728990e64808b3
DucksAreMammals <34693858+DucksAreMammals@users.noreply.github.com>
2021-11-11 23:15:58 -0600

    Added 3rd level

--- Scripts/Enemy.gd
+++ Scripts/Enemy.gd
@@ -22,14 +22,16 @@ func _physics_process(delta):
 		if raycast_left.get_collider().is_in_group("player"):
 			raycast_left.get_collider().hit(-1)
 			direction = 1
-		elif raycast_left.get_collider().is_in_group("enemybox"):
+		elif raycast_left.get_collider().is_in_group("enemybox") or \
+			raycast_left.get_collider().is_in_group("enemy"):
 			direction = 1
 
 	if raycast_right.is_colliding():
 		if raycast_right.get_collider().is_in_group("player"):
 			raycast_right.get_collider().hit(1)
 			direction = -1
-		elif raycast_right.get_collider().is_in_group("enemybox"):
+		elif raycast_right.get_collider().is_in_group("enemybox") or \
+			raycast_right.get_collider().is_in_group("enemy"):
 			direction = -1
 
 	velocity.x = direction * speed
--- Scripts/ParticlesCache.gd
+++ Scripts/ParticlesCache.gd
@@ -10,7 +10,7 @@ var materials = [
 func _ready():
 	for material in materials:
 		var particles_instance = Particles2D.new()
-		particles_instance.position = Vector2(110, 110)
+		particles_instance.position = Vector2(0, 0)
 		particles_instance.set_process_material(material)
 		particles_instance.set_one_shot(true)
 		particles_instance.set_emitting(true)
