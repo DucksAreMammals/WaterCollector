e80961cb38f7000426780e3e6b3565933145fd85
DucksAreMammals <34693858+DucksAreMammals@users.noreply.github.com>
2021-11-12 21:42:01 -0600

    Fixed typo

--- Scripts/LevelEndPopup.gd
+++ Scripts/LevelEndPopup.gd
@@ -3,14 +3,14 @@ extends CanvasLayer
 var just_hidden = false
 
 func _process(_delta):
-	if $LooseDialog.visible and Input.is_action_pressed("continue"):
+	if $LoseDialog.visible and Input.is_action_pressed("continue"):
 		_on_RestartButton_pressed()
 	elif $WinDialog.visible and Input.is_action_pressed("continue"):
 		_on_NextLevelButton_pressed()
 	
 	if just_hidden:
 		$WinDialog.hide()
-		$LooseDialog.hide()
+		$LoseDialog.hide()
 	
 	just_hidden = false
 
@@ -21,14 +21,14 @@ func on_win():
 
 
 func on_loose():
-	$LooseDialog.popup_centered()
+	$LoseDialog.popup_centered()
 	get_tree().paused = true
 
 
 func _on_NextLevelButton_pressed():
 	just_hidden = true
 	$WinDialog.hide()
-	$LooseDialog.hide()
+	$LoseDialog.hide()
 # warning-ignore:return_value_discarded
 	get_tree().change_scene("res://Scenes/Levels/" + str(Global.level) + ".tscn")
 
@@ -37,14 +37,14 @@ func _on_WinDialog_popup_hide():
 	get_tree().paused = false
 
 
-func _on_LooseDialog_popup_hide():
+func _on_LoseDialog_popup_hide():
 	get_tree().paused = false
 
 
 func _on_RestartButton_pressed():
 	just_hidden = true
 	$WinDialog.hide()
-	$LooseDialog.hide()
+	$LoseDialog.hide()
 # warning-ignore:return_value_discarded
 	get_tree().reload_current_scene()
 
@@ -52,6 +52,6 @@ func _on_RestartButton_pressed():
 func _on_MenuButton_pressed():
 	just_hidden = true
 	$WinDialog.hide()
-	$LooseDialog.hide()
+	$LoseDialog.hide()
 # warning-ignore:return_value_discarded
 	get_tree().change_scene("res://Scenes/MainMenu.tscn")
