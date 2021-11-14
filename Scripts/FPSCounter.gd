4ba0bf4f9789854a4a3581476f96ff054eeeacb2
DucksAreMammals <34693858+DucksAreMammals@users.noreply.github.com>
2021-11-13 18:12:36 -0600

    Fixed fps counter position

--- Scripts/FPSCounter.gd
+++ Scripts/FPSCounter.gd
@@ -4,6 +4,6 @@ extends CanvasLayer
 func _process(_delta):
 	if Global.show_fps:
 		$Label.visible = true
-		$Label.text = "FPS: " + str(Performance.get_monitor(Performance.TIME_FPS))
+		$Label.text = str(Performance.get_monitor(Performance.TIME_FPS))
 	else:
 		$Label.visible = false
