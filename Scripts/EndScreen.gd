be15b7eee45ad47af2479d7cff3c07bf605faeba
DucksAreMammals <34693858+DucksAreMammals@users.noreply.github.com>
2021-11-17 03:33:39 -0600

    level -1 now grabs focus to the menu button

--- Scripts/EndScreen.gd
+++ Scripts/EndScreen.gd
@@ -1,5 +1,8 @@
 extends Control
 
+func _ready():
+	$VBoxContainer/MenuButton.grab_focus()
+
 func _on_MenuButton_pressed():
 # warning-ignore:return_value_discarded
 	get_tree().change_scene("res://Scenes/MainMenu.tscn")
