c3b22b5da91b3bffbacb940328675f1c6a0bc369
DucksAreMammals <34693858+DucksAreMammals@users.noreply.github.com>
2021-11-17 14:43:50 -0600

    Added good and bad ending

--- Scripts/EndScreen.gd
+++ Scripts/EndScreen.gd
@@ -2,6 +2,11 @@ extends Control
 
 func _ready():
 	$VBoxContainer/MenuButton.grab_focus()
+	
+	if Global.good_ending:
+		$VBoxContainer/Label3.text = "You pressed the water reset switch and saved everybody!"
+	else:
+		$VBoxContainer/Label3.text = "You didn't press the water reset button so everyone is still dehydrated."
 
 func _on_MenuButton_pressed():
 # warning-ignore:return_value_discarded
